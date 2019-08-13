81ff2a7d041c333d3c038393b2ce52e48c43f9fd
Adam Spice <adam@namsys.com.au>
2019-08-13 12:08:04 +1200

    Updates

---
+++ precache-manifest.5a6729b0734e7da8ea3be7c255d63b06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fc6244446feef045afdcf84ffd4d69",
+    "url": "/monsters-rollodex/index.html"
+  },
+  {
+    "revision": "dbcf17b2f6be72e2fab9",
+    "url": "/monsters-rollodex/static/css/main.f1054543.chunk.css"
+  },
+  {
+    "revision": "0b39153bc2f9c7ae2483",
+    "url": "/monsters-rollodex/static/js/2.7f9914d1.chunk.js"
+  },
+  {
+    "revision": "dbcf17b2f6be72e2fab9",
+    "url": "/monsters-rollodex/static/js/main.fd305146.chunk.js"
+  },
+  {
+    "revision": "cbd92bba02d578376bd4",
+    "url": "/monsters-rollodex/static/js/runtime~main.5cc2bdc0.js"
+  }
+]);
